0ba2b300e8f42b1597f3f2873775c944e588dd2c
choedayeon <cdy2382@gmail.com>
2025-03-20 21:02:37 +0900

    음

--- lib/ui/pages/splash.dart
+++ lib/ui/pages/splash.dart
@@ -9,14 +9,30 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin{
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
   late final AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 1500),);
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    // 애니메이션이 끝나면 자동으로 다음 화면으로 이동
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _navigateToNextScreen();
+      }
+    });
+  }
+
+  void _navigateToNextScreen() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const NextPage()), // 다음 페이지로 이동
+    );
   }
 
   @override
@@ -27,23 +43,36 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100.w,
-      height: 100.h,
-      child:Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [Lottie.network(
-        "https://lottie.host/40ac0f4e-76ed-43dc-8306-f4e3eeac34f1/b8xKHZtuqR.json",
-        controller: _controller,
-        onLoaded: (composition) {
-              _controller
-                ..duration = composition.duration
-                ..forward();
-            },
-        width: 80.w,
+    return Scaffold(
+      body: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬 추가
+          children: [
+            Lottie.network(
+              "https://lottie.host/1b8200fd-39c7-41cf-ac5f-de15715a4fff/ug4cz6Eq75.json",
+              controller: _controller,
+              onLoaded: (composition) {
+                _controller
+                  ..duration = composition.duration
+                  ..forward();
+              },
+              width: 80.w,
+            ),
+          ],
+        ),
       ),
-      ],
-    )
+    );
+  }
+}
+
+// 예제용 다음 페이지
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Next Page', style: TextStyle(fontSize: 24))),
     );
   }
 }
