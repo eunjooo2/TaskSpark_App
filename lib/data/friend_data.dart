f1a35873cd7901f60595e7425368257f7bc51990
choedayeon <cdy2382@gmail.com>
2025-04-30 01:14:29 +0900

    :memo: friend dummy data 추가

---
+++ lib/data/friend_data.dart
@@ -0,0 +1,95 @@
+import 'package:task_spark/utils/models/friend.dart';
+
+final List<Map<String, dynamic>> friendDummyData = [
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_9",
+    "receiver": "user_18",
+    "status": "pending",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_4",
+    "receiver": "user_13",
+    "status": "rejected",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_5",
+    "receiver": "user_20",
+    "status": "blocked",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_9",
+    "receiver": "user_12",
+    "status": "rejected",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_4",
+    "receiver": "user_11",
+    "status": "accepted",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_2",
+    "receiver": "user_17",
+    "status": "accepted",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_5",
+    "receiver": "user_20",
+    "status": "rejected",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_10",
+    "receiver": "user_13",
+    "status": "rejected",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_6",
+    "receiver": "user_16",
+    "status": "blocked",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_5",
+    "receiver": "user_16",
+    "status": "blocked",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_9",
+    "receiver": "user_12",
+    "status": "rejected",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_8",
+    "receiver": "user_16",
+    "status": "accepted",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_9",
+    "receiver": "user_16",
+    "status": "accepted",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_5",
+    "receiver": "user_20",
+    "status": "accepted",
+  },
+  {
+    "collectionId": "friend_requests",
+    "sender": "user_2",
+    "receiver": "user_11",
+    "status": "accepted",
+  },
+];
+// json generator를 사용한 더미 데이터
