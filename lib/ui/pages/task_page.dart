345026a721a8bd60c728d759d3443c92a3a5fd0d
rlaxogh5079 <rlaxogh507906@gmail.com>
2025-06-07 07:41:26 +0900

    :bug: setState() after dispose() 오류 수정

--- lib/ui/pages/task_page.dart
+++ lib/ui/pages/task_page.dart
@@ -46,12 +46,17 @@ class _TaskPageState extends State<TaskPage> {
   }
 
   Future<void> _fetchData() async {
-    print(await _taskService.getTaskGoalCount(
-        await SecureStorage().storage.read(key: "userID") ?? ""));
+    final userID = await SecureStorage().storage.read(key: "userID") ?? "";
+    print(await _taskService.getTaskGoalCount(userID));
+
+    if (!mounted) return;
     setState(() => _isLoading = true);
+
     try {
       final categories = await _categoryService.getAllCategories();
       final tasks = await _taskService.getAllTasks();
+
+      if (!mounted) return;
       setState(() {
         _categories = categories;
         _tasks = tasks;
@@ -59,6 +64,7 @@ class _TaskPageState extends State<TaskPage> {
     } catch (_) {
       _showSnackBar("데이터 불러오기 실패 😥");
     } finally {
+      if (!mounted) return;
       setState(() => _isLoading = false);
     }
   }
@@ -99,10 +105,13 @@ class _TaskPageState extends State<TaskPage> {
     try {
       if (task.isDone == true) return;
       if (task.startDate != null &&
-          task.startDate!.isAfter(DateTime.now().add(Duration(hours: 9))))
+          task.startDate!.isAfter(DateTime.now().add(Duration(hours: 9)))) {
         return;
+      }
+
       await _taskService.handleTaskCompletion(task);
       _showSnackBar("할 일 완료! 경험치가 지급되었습니다 🎉");
+
       await _fetchData();
     } catch (e) {
       _showSnackBar("완료 처리 실패: $e");
